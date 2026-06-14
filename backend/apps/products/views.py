970b728f98c9998c54bd469da425db116c123e7b
Jaicker Rafael Lozano Flores <jlozano.devcode@gmail.com>
2026-06-14 17:42:16 -0400

    feat(products): agregando lógica de entrada y creacion de movimiento si se crea un producto

--- backend/apps/products/views.py
+++ backend/apps/products/views.py
@@ -1,4 +1,6 @@
+from django.db import transaction
 from rest_framework import viewsets
+from rest_framework.response import Response 
 from .serializers import ProductSerializer, SupplierSerializer, CategorySerializer, StockMovementSerializer
 from .models import Product, Supplier, Category, StockMovement
 
@@ -6,6 +8,33 @@ from .models import Product, Supplier, Category, StockMovement
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    def create(self, request, *args, **kwargs):
+        """
+        Crear un producto y registrar un movimiento de stock inicial si se proporciona current_stock
+        """
+        initial_stock = int(request.data.get('current_stock', 0))
+
+        if initial_stock and int(initial_stock) < 0:
+            raise serializers.ValidationError({
+                "current_stock": "El stock inicial no puede ser menor que cero."
+            })
+        
+        # Creamos el producto usando el serializer normal
+        with transaction.atomic():
+            serializer = self.get_serializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            product = serializer.save()  # Guardamos el producto para obtener su ID
+
+            # Si se proporcionó un stock inicial mayor a 0, registramos un movimiento de stock IN
+            if initial_stock and int(initial_stock) > 0:
+                StockMovement.objects.create(
+                    product=product,
+                    type='IN',
+                    quantity=int(initial_stock)
+                )
+
+        return Response(serializer.data, status=201)
 
 
 class SupplierViewSet(viewsets.ModelViewSet):
