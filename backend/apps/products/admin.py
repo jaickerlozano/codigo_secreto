2f6fc4c046340bf556036eb2db375d790cb49308
Jaicker Rafael Lozano Flores <jlozano.devcode@gmail.com>
2026-06-16 21:05:17 -0400

    feat(products): eliminando configuración del panel desde esta aplicación

--- backend/apps/products/admin.py
+++ backend/apps/products/admin.py
@@ -2,23 +2,23 @@ from django.contrib import admin
 from .models import Product, Supplier, Category, StockMovement
 
 # Register your models here.
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'category', 'supplier', 'current_stock', 'minimum_stock', 'price')
-    search_fields = ('name',)
-    list_filter = ('category', 'supplier')
+# @admin.register(Product)
+# class ProductAdmin(admin.ModelAdmin):
+#     list_display = ('name', 'category', 'supplier', 'current_stock', 'minimum_stock', 'price')
+#     search_fields = ('name',)
+#     list_filter = ('category', 'supplier')
 
-@admin.register(Supplier)
-class SupplierAdmin(admin.ModelAdmin):
-    list_display = ('name', 'contact')
-    search_fields = ('name',)
+# @admin.register(Supplier)
+# class SupplierAdmin(admin.ModelAdmin):
+#     list_display = ('name', 'contact')
+#     search_fields = ('name',)
 
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-    search_fields = ('name',)
+# @admin.register(Category)
+# class CategoryAdmin(admin.ModelAdmin):
+#     list_display = ('name',)
+#     search_fields = ('name',)
 
-@admin.register(StockMovement)
-class StockMovementAdmin(admin.ModelAdmin):
-    list_display = ('product', 'type', 'quantity', 'timestamp')
-    list_filter = ('type', 'timestamp')
+# @admin.register(StockMovement)
+# class StockMovementAdmin(admin.ModelAdmin):
+#     list_display = ('product', 'type', 'quantity', 'timestamp')
+#     list_filter = ('type', 'timestamp')
