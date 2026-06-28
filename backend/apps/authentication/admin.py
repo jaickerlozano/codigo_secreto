44ae1036af19ad680a5147b84681cd783eac2325
Jaicker Rafael Lozano Flores <jlozano.devcode@gmail.com>
2026-06-28 14:48:25 -0400

    feat(authentication): agregando nueva vista para visualizar los perfiles de clientes

--- backend/apps/authentication/admin.py
+++ backend/apps/authentication/admin.py
@@ -23,3 +23,17 @@ class UserAdmin(BaseUserAdmin):
         ('Permisos y Roles', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
         ('Fechas Importantes', {'fields': ('last_login', 'date_joined')}),
     )
+
+
+@admin.register(CustomerProfile)
+class CustomerProfileAdmin(admin.ModelAdmin):
+    # CORRECCIÓN: Cambiamos 'comuna' por 'province' y 'birthday' por 'birth_date'
+    list_display = ('id', 'user_email', 'region', 'province', 'address', 'birth_date')
+    
+    # CORRECCIÓN: Cambiamos 'comuna' por 'province' para que coincida con el campo de tu modelo
+    list_filter = ('region', 'province')
+    search_fields = ('user__email', 'address')
+
+    def user_email(self, obj):
+        return obj.user.email
+    user_email.short_description = "Correo del Cliente"
